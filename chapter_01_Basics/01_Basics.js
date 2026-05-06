7be7862696c1ff799d6f525f5907ca7f402c1c30
Suraj <surajkulenka@gmail.com>
2026-05-06 23:08:08 +0530

    Initial commit from LearnPlaywright workspace

---
+++ chapter_01_Basics/01_Basics.js
@@ -0,0 +1,16 @@
+console.log("Hello,The Testing Academy!");
+
+let a = 10;
+console.log(a);
+
+For (let a = 0; a<100000; a++)
+{
+    console.log(a);
+    print();
+}
+
+function print()
+{
+    console.log("Hello");
+}
+
---
+++ chapter_01_Basics/03_JS_Verify_Setup.js
@@ -0,0 +1,4 @@
+console.log(process.platform); // o/p: win32
+console.log(process.arch); // o/p: x64
+console.log(process.version); // o/p: v24.15.0
+console.log("Node Version:", process.version); // o/p: Node Version: v24.15.0
